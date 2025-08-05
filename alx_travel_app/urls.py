67a95c11124954c64fc1ca393474c6d55a5f7235
Jonas Levis <s80programmeomega@gmail.com>
2025-08-05 18:27:20 +0100

    configured rest framework, cors headers, database and swagger documentation

--- alx_travel_app/urls.py
+++ alx_travel_app/urls.py
@@ -14,9 +14,37 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from os import name
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Travel app API",
+        default_version="v1",
+        description="API documentation for the alx_travel_app project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(name="Jonas Levis",email="jonasinfo2016@gmail.com"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path(
+        "swagger.<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
